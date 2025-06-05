574b150b24d0142095a98c62da7194a6fe324528
Alexander-Agu <theonlyagu@gmail.com>
2025-06-05 12:43:11 +0200

    Added basic hero section

---
+++ Client/src/components/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./hero.css"
+
+export default function Hero() {
+  return (
+    <section className='hero'>
+        <h1>Master Your <span className='gradient-text'>Inventory</span></h1>
+
+        <p>
+            Streamline your inventory management with Invi-Tory's powerful, secure platform. Built with ASP.NET and SQL Server for enterprise-grade performance and reliability.
+        </p>
+
+        <nav className='heroNav'>
+            <a href="#" className='heroLink'>Join us today</a>
+            <a href="#" className='heroLink request'>Request Demo</a>
+        </nav>
+    </section>
+  )
+}
