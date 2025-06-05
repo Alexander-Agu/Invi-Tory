d2e5d4ec824c34160d84173dd83ae93d5f7badc4
Alexander-Agu <theonlyagu@gmail.com>
2025-06-05 12:14:21 +0200

    Finished the basic header component

--- Client/src/components/Header/Header.jsx
+++ Client/src/components/Header/Header.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import "./header.css"
+import { BsBoxSeam } from "react-icons/bs";
 
 export default function Header() {
   return (
     <header className='header'>
         <div className="headerOverlap">
             <div className="logo">
-                <h2>Invi-Tory</h2>
+                <a href="#">
+                    <BsBoxSeam style={{scale: 1.5, fontWeight: "bolder", color: "#2563eb"}}/>
+                    <h2>Invi-Tory</h2>
+                </a> 
             </div>
 
             <nav className='navbar'>
