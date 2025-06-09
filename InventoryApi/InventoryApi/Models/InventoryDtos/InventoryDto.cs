c968ed79fb042028f700ffdfb4cb1b167fb4c2b0
Alexander-Agu <theonlyagu@gmail.com>
2025-06-09 12:04:48 +0200

    Fixing remaining dtos

--- InventoryApi/InventoryApi/Models/InventoryDtos/InventoryDto.cs
+++ InventoryApi/InventoryApi/Models/InventoryDtos/InventoryDto.cs
@@ -3,6 +3,7 @@
     public class InventoryDto
     {
         public int InventoryId { get; set; }
+        public int UserId { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Category { get; set; } = string.Empty;
     }
