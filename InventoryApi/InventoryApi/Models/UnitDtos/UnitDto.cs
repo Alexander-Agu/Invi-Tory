ad6ce3797e8e93e87f2b0d896ecb0b4d1a336d3e
Alexander-Agu <theonlyagu@gmail.com>
2025-06-09 12:01:00 +0200

    Created Unit Dtos

---
+++ InventoryApi/InventoryApi/Models/UnitDtos/UnitDto.cs
@@ -0,0 +1,10 @@
+﻿namespace InventoryApi.Models.UnitDtos
+{
+    public class UnitDto
+    {
+        public int UnitId { get; set; }
+        public int UserId { get; set; }
+        public int InventoryUnit { get; set; }
+        public int ItemUnit { get; set; }
+    }
+}
