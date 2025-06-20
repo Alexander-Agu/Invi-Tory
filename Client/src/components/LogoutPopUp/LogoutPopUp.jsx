ab13be7ea93bf7caa0e023a9d2b79191dd828ec4
Alexander-Agu <theonlyagu@gmail.com>
2025-06-20 07:56:29 +0200

    Fixed the logout popup

--- Client/src/components/LogoutPopUp/LogoutPopUp.jsx
+++ Client/src/components/LogoutPopUp/LogoutPopUp.jsx
@@ -4,7 +4,7 @@ import "./logoutPopUp.css"
 export default function LogoutPopUp({setLogoutPopUp}) {
   return (
     <div className="log-pop-up" onClick={()=> setLogoutPopUp(false)}>
-        <div className="logout-app">
+        <div className="logout-app" onClick={(e) => e.stopPropagation()}>
             <h1>Are you sure you want to log out?</h1>
 
             <div className="logout-buttons">
