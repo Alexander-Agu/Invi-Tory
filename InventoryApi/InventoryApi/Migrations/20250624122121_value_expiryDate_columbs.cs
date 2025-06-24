2a4dbad2496a373a1efcb621eec4feccefab93fa
Alexander-Agu <theonlyagu@gmail.com>
2025-06-24 15:16:50 +0200

    adding new migrations

---
+++ InventoryApi/InventoryApi/Migrations/20250624122121_value_expiryDate_columbs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventoryApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class value_expiryDate_columbs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "ExpiryDate",
+                table: "Items",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExpiryDate",
+                table: "Items");
+        }
+    }
+}
---
+++ InventoryApi/InventoryApi/Migrations/20250624124603_remove_expiryDate_columb.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventoryApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class remove_expiryDate_columb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExpiryDate",
+                table: "Items");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "ExpiryDate",
+                table: "Items",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1));
+        }
+    }
+}
