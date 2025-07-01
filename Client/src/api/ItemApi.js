e5280268472d1f3183356ab0d1ed12f15f4ef141
Alexander-Agu <theonlyagu@gmail.com>
2025-07-01 23:36:07 +0200

    Adding authorization headers to my Items axios requests

--- Client/src/api/ItemApi.js
+++ Client/src/api/ItemApi.js
@@ -1,11 +1,19 @@
 import axios from "axios";
 
+const token = JSON.parse(localStorage.getItem("user")).accessToken;
+
+const api = axios.create({
+    baseURL: "https://localhost:7216/api",
+    headers: {
+        Authorization: `Bearer ${token}`
+    }
+})
 
 
 // Get all items
 export const GetAllItemsAsync = async (userId) => {
     try {
-        const res = await axios.get(`https://localhost:7216/api/Item/get-all-items/${userId}`)
+        const res = await api.get(`/Item/get-all-items/${userId}`)
         return res.data;
     } catch (error) {
         console.log(error);
@@ -16,7 +24,7 @@ export const GetAllItemsAsync = async (userId) => {
 // Create item
 export const CreateItemAsync = async (userId, inventoryId, body) => {
     try {
-        const res = await axios.post(`https://localhost:7216/api/Item/create-item/${userId}/${inventoryId}`, body);
+        const res = await api.post(`/Item/create-item/${userId}/${inventoryId}`, body);
 
         return res.data;
     } catch (error) {
@@ -27,7 +35,7 @@ export const CreateItemAsync = async (userId, inventoryId, body) => {
 // Delete item
 export const DeleteItemAsync = async (userId, inventoryId, itemId) => {
     try {
-        const res = await axios.delete(`https://localhost:7216/api/Item/delete-item/${userId}/${inventoryId}/${itemId}`);
+        const res = await api.delete(`/Item/delete-item/${userId}/${inventoryId}/${itemId}`);
 
         return res.data;
     } catch (error) {
@@ -38,7 +46,7 @@ export const DeleteItemAsync = async (userId, inventoryId, itemId) => {
 // Update item
 export const UpdateItemAsync = async (userId, inventoryId, itemId, body) => {
     try {
-        const res = await axios.put(`https://localhost:7216/api/Item/update-item/${userId}/${inventoryId}/${itemId}`, body);
+        const res = await api.put(`/Item/update-item/${userId}/${inventoryId}/${itemId}`, body);
 
         return res.data;
     } catch (error) {
